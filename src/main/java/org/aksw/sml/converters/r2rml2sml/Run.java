5bc0afcf2f8efa96a5d6cc7a7a3aee99f8e0560b
Patrick Westphal <patrick.westphal@teebeutelstoerche.de>
2014-07-26 19:27:09 +0200

    added main structure

---
+++ src/main/java/org/aksw/sml/converters/r2rml2sml/Run.java
@@ -0,0 +1,10 @@
+package org.aksw.sml.converters.r2rml2sml;
+
+public class Run {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/main/java/org/aksw/sml/converters/sml2r2rml/Run.java
@@ -0,0 +1,10 @@
+package org.aksw.sml.converters.sml2r2rml;
+
+public class Run {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
