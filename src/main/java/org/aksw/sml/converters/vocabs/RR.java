49c99711275f6dde15e88b067d7cab453bb2862c
Patrick Westphal <patrick.westphal@teebeutelstoerche.de>
2014-07-27 00:40:32 +0200

    added rr vocabulary URI constants

---
+++ src/main/java/org/aksw/sml/converters/vocabs/RR.java
@@ -0,0 +1,52 @@
+package org.aksw.sml.converters.vocabs;
+
+public class RR {
+    public static final String prefix = "http://www.w3.org/ns/r2rml#";
+    // R2RML classes
+    public static final String BaseTableOrView = prefix + "BaseTableOrView";
+    public static final String GraphMap = prefix + "GraphMap";
+    public static final String Join = prefix + "Join";
+    public static final String LogicalTable = prefix + "LogicalTable";
+    public static final String ObjectMap = prefix  + "ObjectMap";
+    public static final String PredicateMap = prefix + "PredicateMap";
+    public static final String PredicateObjectMap = prefix + "PredicateObjectMap";
+    public static final String R2RMLView = prefix + "R2RMLView";
+    public static final String RefObjectMap = prefix + "RefObjectMap";
+    public static final String SubjectMap = prefix + "SubjectMap";
+    public static final String TermMap = prefix + "TermMap";
+    public static final String TriplesMap = prefix + "TriplesMap";
+
+    // R2RML properties
+    public static final String child = prefix + "child";
+    public static final String class_ = prefix + "class";
+    public static final String column = prefix + "column";
+    public static final String datatype = prefix + "datatype";
+    public static final String constant = prefix  + "constant";
+    public static final String graph = prefix + "graph";
+    public static final String graphMap = prefix + "graphMap";
+    public static final String inverseExpression = prefix + "inverseExpression";
+    public static final String joinCondition = prefix + "joinCondition";
+    public static final String language = prefix + "language";
+    public static final String logicalTable = prefix + "logicalTable";
+    public static final String object = prefix + "object";
+    public static final String objectMap = prefix + "objectMap";
+    public static final String parent = prefix + "parent";
+    public static final String parentTriplesMap = prefix + "parentTriplesMap";
+    public static final String predicate = prefix + "predicate";
+    public static final String predicateMap = prefix + "predicateMap";
+    public static final String predicateObjectMap = prefix + "predicateObjectMap";
+    public static final String sqlQuery = prefix + "sqlQuery";
+    public static final String sqlVersion = prefix + "sqlVersion";
+    public static final String subject = prefix + "subject";
+    public static final String subjectMap = prefix + "subjectMap";
+    public static final String tableName = prefix + "tableName";
+    public static final String template = prefix + "template";
+    public static final String termType = prefix + "termType";
+
+    // other terms
+    public static final String defaultGraph = prefix + "defaultGraph";
+    public static final String SQL2008 = prefix + "SQL2008";
+    public static final String IRI = prefix + "IRI";
+    public static final String BlankNode = prefix + "BlankNode";
+    public static final String Literal = prefix + "Literal";
+}
