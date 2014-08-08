40e57dccb88a796d90d1a4c24d3e1d20829cd2f2
Patrick Westphal <patrick.westphal@teebeutelstoerche.de>
2014-08-08 18:42:17 +0200

    added TermMap base class and made SubjectMap inherit from it

--- src/main/java/org/aksw/sml/converters/r2rml2sml/SubjectMap.java
+++ src/main/java/org/aksw/sml/converters/r2rml2sml/SubjectMap.java
@@ -4,24 +4,19 @@ import java.util.Set;
 
 import org.aksw.sml.converters.vocabs.RR;
 
+import com.hp.hpl.jena.rdf.model.Literal;
 import com.hp.hpl.jena.rdf.model.Model;
 import com.hp.hpl.jena.rdf.model.RDFNode;
 import com.hp.hpl.jena.rdf.model.Resource;
 
-public class SubjectMap {
+public class SubjectMap extends TermMap {
 
-    private Model model;
-    private Resource subject;
-
-    /** @author sherif */
-    public SubjectMap(Model model, Resource subject) {
-        super();
-        this.model = model;
-        this.subject = subject;
+    public SubjectMap(Model model, Resource subjectMapResource) {
+        super(model, subjectMapResource);
     }
 
     public RDFNode getRrClass() {
-        Set<RDFNode> objects = model.listObjectsOfProperty(subject,RR.class_).toSet();
+        Set<RDFNode> objects = model.listObjectsOfProperty(resource,RR.class_).toSet();
 
         if(objects.isEmpty()) {
             return null;
@@ -32,88 +27,26 @@ public class SubjectMap {
         return node;
     }
 
+    /**
+     * @deprecated use getTemplateTerm().getLexicalForm() instead (and also check if null)
+     */
+    @Deprecated
     public String getTemplate() {
-        Set<RDFNode> objects = model.listObjectsOfProperty(subject, RR.template).toSet();
+        Literal templateTerm = getTemplateTerm();
 
-        if(objects.isEmpty()) {
+        if (templateTerm == null) {
             return null;
+        } else {
+            return templateTerm.getLexicalForm();
         }
-
-        RDFNode node = RRUtils.getFirst(objects);
-        String result = "" + node.asNode().getLiteralValue();
-
-        return result;
-    }
-
-    /* (non-Javadoc)
-     * @see java.lang.Object#toString()
-     */
-    @Override
-    public String toString() {
-        return ""+ subject;
-    }
-
-    /* (non-Javadoc)
-     * @see java.lang.Object#hashCode()
-     */
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((model == null) ? 0 : model.hashCode());
-        result = prime * result + ((subject == null) ? 0 : subject.hashCode());
-        return result;
-    }
-
-    /* (non-Javadoc)
-     * @see java.lang.Object#equals(java.lang.Object)
-     */
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) return true;
-        if (obj == null) return false;
-        if (getClass() != obj.getClass()) return false;
-
-        SubjectMap other = (SubjectMap) obj;
-
-        if (model == null) {
-            if (other.model != null) return false;
-
-        } else if (!model.equals(other.model)) return false;
-
-        if (subject == null) {
-            if (other.subject != null) return false;
-
-        } else if (!subject.equals(other.subject)) return false;
-
-        return true;
     }
 
     /**
-     * @return the model
-     */
-    public Model getModel() {
-        return model;
-    }
-
-    /**
-     * @param model the model to set
-     */
-    public void setModel(Model model) {
-        this.model = model;
-    }
-
-    /**
+     * @deprecated use getResource() instead
      * @return the subject
      */
+    @Deprecated
     public Resource getSubject() {
-        return subject;
-    }
-
-    /**
-     * @param subject the subject to set
-     */
-    public void setSubject(Resource subject) {
-        this.subject = subject;
+        return resource;
     }
 }
---
+++ src/main/java/org/aksw/sml/converters/r2rml2sml/TermMap.java
@@ -0,0 +1,186 @@
+package org.aksw.sml.converters.r2rml2sml;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.aksw.sml.converters.vocabs.RR;
+
+import com.hp.hpl.jena.rdf.model.Literal;
+import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.rdf.model.Property;
+import com.hp.hpl.jena.rdf.model.RDFNode;
+import com.hp.hpl.jena.rdf.model.Resource;
+import com.hp.hpl.jena.rdf.model.Statement;
+import com.hp.hpl.jena.rdf.model.StmtIterator;
+
+public abstract class TermMap {
+
+    protected Model model;
+    protected Resource resource;
+    protected Property termMapProperty = null;
+
+    private Resource datatype;
+    private boolean datatypeChecked;
+    private Literal language;
+    private boolean languageChecked;
+
+    public TermMap(Model model, Resource termMapResource) {
+        this.model = model;
+        this.resource = termMapResource;
+        setTermMapProperty();
+        datatype = null;
+        datatypeChecked = false;
+        language = null;
+        languageChecked = false;
+    }
+
+    private void setTermMapProperty() {
+        List<Property> mapTypeStatements = Arrays.asList(RR.column,
+                RR.template, RR.constant);
+
+        StmtIterator sttmntIt = model.listStatements(resource, null, (RDFNode) null);
+
+        while(sttmntIt.hasNext()) {
+            Statement statement = sttmntIt.next();
+            Property prop = statement.getPredicate();
+            if (mapTypeStatements.contains(prop)) {
+                termMapProperty = prop;
+                break;
+            }
+        }
+        if (termMapProperty == null) {
+            throw new RuntimeException("There should be a term map property " +
+                    "but none is contained in the current term map model: " +
+                    model.toString());
+        }
+    }
+
+    public boolean isConstantTermMap() {
+        return termMapProperty.equals(RR.constant);
+    }
+
+    public boolean isTemplateTermMap() {
+        return termMapProperty.equals(RR.template);
+    }
+
+    public boolean isColumnTermMap() {
+        return termMapProperty.equals(RR.column);
+    }
+
+    public Resource getResource() {
+        return resource;
+    }
+
+    public Model getModel() {
+        return model;
+    }
+
+    public RDFNode getConstantTerm() {
+        RDFNode term = RRUtils.getNodeFromSet(model.listObjectsOfProperty(
+                resource, RR.template).toSet());
+        return term;
+    }
+
+    public Literal getColumnTerm() {
+        RDFNode term = RRUtils.getNodeFromSet(model.listObjectsOfProperty(
+                resource, RR.template).toSet());
+        return term.asLiteral();
+    }
+
+    public Literal getTemplateTerm() {
+        RDFNode term = RRUtils.getNodeFromSet(model.listObjectsOfProperty(
+                resource, RR.template).toSet());
+        return term.asLiteral();
+    }
+
+    public RDFNode getTerm() {
+        if (isConstantTermMap()) {
+            return getConstantTerm();
+
+        } else if (isColumnTermMap()) {
+            return getColumnTerm();
+
+        } else if (isTemplateTermMap()) {
+            return getTemplateTerm(); 
+
+        } else {
+            return null;
+        }
+    }
+
+    public Literal getLanguage() {
+        if (languageChecked) {
+            return language;
+
+        } else {
+            Literal lang = (Literal) RRUtils.getNodeFromSetIfExists(model
+                    .listObjectsOfProperty(resource, RR.language).toSet());
+            language = lang;
+            languageChecked = true;
+            return lang;
+        }
+    }
+
+    public boolean hasLanguage() {
+        return getLanguage() != null;
+    }
+
+    public Resource getDatatype() {
+        if (datatypeChecked) {
+            return datatype;
+        } else {
+            Resource datatype = (Resource) RRUtils.getNodeFromSetIfExists(model
+                    .listObjectsOfProperty(resource, RR.datatype).toSet());
+            this.datatype = datatype;
+            datatypeChecked = true;
+            return datatype;
+        }
+    }
+
+    public boolean hasDatatype() {
+        return getDatatype() != null;
+    }
+    /* (non-Javadoc)
+     * @see java.lang.Object#equals(java.lang.Object)
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null) return false;
+        if (getClass() != obj.getClass()) return false;
+
+        TermMap other = (TermMap) obj;
+
+        if (model == null) {
+            if (other.model != null) return false;
+
+        } else if (!model.equals(other.model)) return false;
+
+        if (resource == null) {
+            if (other.resource != null) return false;
+
+        } else if (!resource.equals(other.resource)) return false;
+
+        return true;
+    }
+
+    /* (non-Javadoc)
+     * @see java.lang.Object#hashCode()
+     */
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((model == null) ? 0 : model.hashCode());
+        result = prime * result + ((resource == null) ? 0 : resource.hashCode());
+        return result;
+    }
+
+    /* (non-Javadoc)
+     * @see java.lang.Object#toString()
+     */
+    @Override
+    public String toString() {
+        return resource.toString();
+    }
+}
